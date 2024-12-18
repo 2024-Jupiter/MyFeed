673717a5f813866eb70a583ebc7e6c0000ca69df
LeeJaehyung <540900@naver.com>
2024-12-18 11:06:14 +0900

    init

---
+++ src/main/java/com/myfeed/MyfeedApplication.java
@@ -0,0 +1,13 @@
+package com.myfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfeed/MyfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
