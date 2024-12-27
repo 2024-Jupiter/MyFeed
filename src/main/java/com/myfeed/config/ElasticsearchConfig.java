cbda78dcc123a8299cb0c06df8933db748e9c00c
9hero <rngnlah@naver.com>
2024-12-27 15:53:33 +0900

    feat: postES Document 추가

--- src/main/java/com/myfeed/config/ElasticsearchConfig.java
+++ src/main/java/com/myfeed/config/ElasticsearchConfig.java
@@ -1,5 +1,8 @@
 package com.myfeed.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -9,60 +12,34 @@ import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 
 @Configuration
-public class ElasticsearchConfig {
+public class ElasticSearchConfig {
 
-	@Value("${spring.elasticsearch.rest.url}")
-	private String elasticsearchUrl;
+    @Value("${spring.elasticsearch.rest.url}")
+    private String elasticsearchUrl;
 
-	@Value("${spring.elasticsearch.rest.username}")
-	private String username;
+    @Value("${spring.elasticsearch.rest.username}")
+    private String username;
 
-	@Value("${spring.elasticsearch.rest.password}")
-	private String password;
+    @Value("${spring.elasticsearch.rest.password}")
+    private String password;
 
-	@Bean
-	public ElasticsearchClient getElasticsearchClient() {
-		RestClientBuilder builder = org.elasticsearch.client.RestClient.builder(HttpHost.create(elasticsearchUrl));
+    @Bean
+    public ElasticsearchClient getElasticsearchClient() {
+        RestClientBuilder builder = RestClient.builder(HttpHost.create(elasticsearchUrl));
 
-		builder.setHttpClientConfigCallback(httpAsyncClientBuilder -> {
-			// 사용자 이름과 비밀번호를 설정
-			final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-			return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-		});
+        builder.setHttpClientConfigCallback(httpAsyncClientBuilder -> {
+            // 사용자 이름과 비밀번호를 설정
+            final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+            return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+        });
 
-		// RestClient 및 ElasticsearchClient 생성
-		RestClient restClient = builder.build();
-		RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
-		return new ElasticsearchClient(transport);
-	}
+        // RestClient 및 ElasticsearchClient 생성
+        RestClient restClient = builder.build();
+        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
 }
 
-// @Configuration
-// public class ElasticsearchConfig extends ElasticsearchConfiguration {
-// 	@Value("${spring.elasticsearch.rest.username}")
-// 	private String username;
-//
-// 	@Value("${spring.elasticsearch.rest.password}")
-// 	private String password;
-//
-// 	@Value("${spring.elasticsearch.rest.url}")
-// 	private String esHost;
-//
-// 	@Override
-// 	public ClientConfiguration clientConfiguration() {
-// 		return ClientConfiguration.builder()
-// 			.connectedTo(esHost)
-// 			.usingSsl() // ssl 사용
-// 			.withBasicAuth(username, password)
-// 			.build();
-// 	}
-// }
--- src/main/java/com/myfeed/model/post/PostEs.java
+++ src/main/java/com/myfeed/model/post/PostEs.java
@@ -1,5 +1,6 @@
 package com.myfeed.model.post;
 
+import com.myfeed.model.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +12,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -19,15 +21,46 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PostEs {
+public class PostEs extends BaseTimeEntity {
     @Id
     private String postId;
 
-    // 게시글 정보
-    @Field(type = FieldType.Object)
-    private Map<String, Object> post;
+    // 글쓴이 정보
+    @Field(type = FieldType.Keyword)
+    private String userId;
 
-    // 댓글
-    @Field(type = FieldType.Nested)
-    private List<Map<String, Object>> comments;
+    @Field(type = FieldType.Keyword)
+    private String userNickName;
+
+    @Field(type = FieldType.Keyword)
+    private String userStatus;
+
+    // 게시글 정보
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String content;
+
+    @Field(type = FieldType.Keyword)
+    private PostCategory category;
+
+    @Field(type = FieldType.Integer)
+    private int viewCount;
+
+    @Field(type = FieldType.Integer)
+    private int likeCount;
+
+    @Field(type = FieldType.Keyword)
+    private BlockStatus blockStatus;
+
+    @Field(type = FieldType.Date)
+    private LocalDateTime blockAt;
+
+    @Field(type = FieldType.Date)
+    private LocalDateTime unBlockAt;
+
+    @Field(type = FieldType.Keyword)
+    private List<String> imageUrls;
+
 }
