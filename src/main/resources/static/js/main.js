d4b041d377a89633eb7c00be03c53e380e9a02a0
UnaeKang <inpou1@naver.com>
2025-01-02 17:18:37 +0900

    Design: common 하위 alertMsg 생성, main 페이지 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -16,6 +16,7 @@
         $('.navbar .dropdown').off('mouseover').off('mouseout');
       }
     }
+
     toggleNavbarMethod();
     $(window).resize(toggleNavbarMethod);
   });
@@ -29,58 +30,58 @@
     }
   });
   $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
     return false;
   });
 
   // Vendor carousel
-  $('.vendor-carousel').owlCarousel({
-    loop: true,
-    margin: 29,
-    nav: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-      0: {
-        items: 2,
-      },
-      576: {
-        items: 3,
-      },
-      768: {
-        items: 4,
-      },
-      992: {
-        items: 5,
-      },
-      1200: {
-        items: 6,
-      },
-    },
-  });
+  // $('.vendor-carousel').owlCarousel({
+  //   loop: true,
+  //   margin: 29,
+  //   nav: false,
+  //   autoplay: true,
+  //   smartSpeed: 1000,
+  //   responsive: {
+  //     0: {
+  //       items: 2,
+  //     },
+  //     576: {
+  //       items: 3,
+  //     },
+  //     768: {
+  //       items: 4,
+  //     },
+  //     992: {
+  //       items: 5,
+  //     },
+  //     1200: {
+  //       items: 6,
+  //     },
+  //   },
+  // });
 
   // Related carousel
-  $('.related-carousel').owlCarousel({
-    loop: true,
-    margin: 29,
-    nav: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      576: {
-        items: 2,
-      },
-      768: {
-        items: 3,
-      },
-      992: {
-        items: 4,
-      },
-    },
-  });
+  // $('.related-carousel').owlCarousel({
+  //   loop: true,
+  //   margin: 29,
+  //   nav: false,
+  //   autoplay: true,
+  //   smartSpeed: 1000,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     576: {
+  //       items: 2,
+  //     },
+  //     768: {
+  //       items: 3,
+  //     },
+  //     992: {
+  //       items: 4,
+  //     },
+  //   },
+  // });
 
   // Product Quantity
   $('.quantity button').on('click', function () {
@@ -99,7 +100,7 @@
   });
 })(jQuery);
 
-const swalConfig = { confirmButtonColor: '#1f9bcf' };
+const swalConfig = {confirmButtonColor: '#1f9bcf'};
 
 // pwd, pwd2 일치여부 확인
 document.getElementById('pwd2').addEventListener('keyup', checkPw);
@@ -135,7 +136,7 @@ function sendNumber() {
   if (!phone) {
     Swal.fire({
       icon: 'warning',
-      text: '휴대폰 번호를 입력해주세요.',
+      text: '휴대폰 번호를 입력해 주세요.',
       ...swalConfig,
     });
     return;
@@ -147,7 +148,7 @@ function sendNumber() {
     $.ajax({
       url: '/api/send-sms/send-one',
       type: 'POST',
-      data: JSON.stringify({ phone: cleanedPhoneNum }),
+      data: JSON.stringify({phone: cleanedPhoneNum}),
       contentType: 'application/json',
       dataType: 'json',
       success: function (data) {
@@ -187,7 +188,7 @@ function sendNumber() {
 
 // 이메일 유효성, 중복 검사
 document
-.getElementById('btnCheckEmail')
+.getElementById('checkEmailBtn')
 .addEventListener('click', isEmailAvailable);
 
 function isEmailAvailable() {
@@ -214,12 +215,14 @@ function isEmailAvailable() {
   }
 
   $.ajax({
-    url: '/api/user/check-email',
+    url: '/api/users/check-email',
     type: 'GET',
-    data: { email: email },
+    data: {email: email},
     dataType: 'json',
-    success: function (data) {
-      if (!data || !data.state) {
+    success: function (response) {
+      console.log(response.data);
+
+      if (!response || !response.data) {
         Swal.fire({
           icon: 'error',
           text: '서버 응답이 올바르지 않습니다.',
@@ -228,21 +231,23 @@ function isEmailAvailable() {
         return;
       }
 
-      if (data.state === 'error') {
+      if (!response.data.success) {
+        console.log(data);
         Swal.fire({
           icon: 'warning',
-          text: data.message,
+          text: response.data.message,
           ...swalConfig,
         });
       }
 
-      if (data.state === 'success') {
+      if (response.data.success) {
         Swal.fire({
           icon: 'success',
-          text: data.message,
+          text: response.data.message,
           showCancelButton: true,
           cancelButtonColor: '#d33',
           confirmButtonText: '사용',
+          cancelButtonText: '취소',
           ...swalConfig,
         }).then((result) => {
           if (result.isConfirmed) {
@@ -268,15 +273,18 @@ function isEmailAvailable() {
 
 // 이메일 재입력 시 중복 검사 버튼 활성화
 document.getElementById('email').addEventListener('input', function () {
-  document.getElementById('checkEmailBtn').removeAttribute('disabled');
-  document
-  .getElementById('checkEmailBtn')
-  .setAttribute('data-available', 'false');
+  if (document.getElementById('checkEmailBtn').getAttribute('data-available')
+      === 'true') {
+    document.getElementById('checkEmailBtn').removeAttribute('disabled');
+    document
+    .getElementById('checkEmailBtn')
+    .setAttribute('data-available', 'false');
+  }
 });
 
 // 닉네임 유효성, 중복 검사
 document
-.getElementById('btnCheckNickname')
+.getElementById('checkNicknameBtn')
 .addEventListener('click', isNicknameAvailable);
 
 function isNicknameAvailable() {
@@ -285,19 +293,19 @@ function isNicknameAvailable() {
   if (!nickname) {
     Swal.fire({
       icon: 'warning',
-      text: '닉네임을 입력해주세요.',
+      text: '닉네임을 입력해 주세요.',
       ...swalConfig,
     });
     return;
   }
 
   $.ajax({
-    url: '/api/user/check-nickname',
+    url: '/api/users/check-nickname',
     type: 'GET',
-    data: { nickname: nickname },
+    data: {nickname: nickname},
     dataType: 'json',
-    success: function (data) {
-      if (!data || !data.state) {
+    success: function (response) {
+      if (!response || !response.data) {
         Swal.fire({
           icon: 'error',
           text: '서버 응답이 올바르지 않습니다.',
@@ -305,20 +313,21 @@ function isNicknameAvailable() {
         });
         return;
       }
-      if (data.state === 'error') {
+      if (!response.data.success) {
         Swal.fire({
           icon: 'warning',
-          text: data.message,
+          text: response.data.message,
           ...swalConfig,
         });
       }
-      if (data.state === 'success') {
+      if (response.data.success) {
         Swal.fire({
           icon: 'success',
-          text: data.message,
+          text: response.data.message,
           showCancelButton: true,
           cancelButtonColor: '#d33',
           confirmButtonText: '사용',
+          cancelButtonText: '취소',
           ...swalConfig,
         }).then((result) => {
           if (result.isConfirmed) {
@@ -344,8 +353,47 @@ function isNicknameAvailable() {
 
 // 닉네임 재입력 시 중복 검사 버튼 활성화
 document.getElementById('nickname').addEventListener('input', function () {
-  document.getElementById('checkNicknameBtn').removeAttribute('disabled');
-  document
-  .getElementById('checkNicknameBtn')
-  .setAttribute('data-available', 'false');
+  if (document.getElementById('checkNicknameBtn').getAttribute('data-available')
+      === 'true') {
+    document.getElementById('checkNicknameBtn').removeAttribute('disabled');
+    document
+    .getElementById('checkNicknameBtn')
+    .setAttribute('data-available', 'false');
+  }
 });
+
+document.getElementById('formSubmit').addEventListener('click', checkForm);
+
+function checkForm() {
+
+  if (document.getElementById('checkEmailBtn').getAttribute('data-available')
+      === 'false') {
+    Swal.fire({
+      icon: 'warning',
+      text: '이메일 중복 확인을 해주세요.',
+      ...swalConfig,
+    });
+    return false;
+  }
+
+  if (document.getElementById('checkNicknameBtn').getAttribute('data-available')
+      === 'false') {
+    Swal.fire({
+      icon: 'warning',
+      text: '닉네임 중복 확인을 해주세요.',
+      ...swalConfig,
+    });
+    return false;
+  }
+
+  if (document.getElementById('pwd').value !== document.getElementById(
+      'pwd2').value) {
+    Swal.fire({
+      icon: 'warning',
+      text: '비밀번호가 일치하지 않습니다.',
+      ...swalConfig,
+    });
+    return false;
+  }
+  document.getElementById('registerForm').submit();
+}
